b62c94ace447bbda34bea4f135ababc1d5f3b006
Delali Funani <dfunani@live.co.za>
2023-01-04 07:32:08 -0800

    added

--- 0x01-python-if_else_loops_functions/2-print_alphabet.py
+++ 0x01-python-if_else_loops_functions/2-print_alphabet.py
@@ -1,5 +1,3 @@
 #!/usr/bin/python3
-for letter in ["a","b","c","d","e","f","g","h","i","j","k","l",
-                             "m","n","o","p","q","r","s","t","u",
-                             "v","w","x","y","z"]:
-    print(letter, end="")
+for i in range(97,123):
+    print(chr(i), end="")
