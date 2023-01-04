95b9277ac828f401976acbd549d608771e9fb558
Delali Funani <dfunani@live.co.za>
2023-01-04 07:41:51 -0800

    added

--- 0x01-python-if_else_loops_functions/3-print_alphabt.py
+++ 0x01-python-if_else_loops_functions/3-print_alphabt.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
-for i in ["a","b","c","d","f","g","h","i","j","k"
-          ,"l","m","n","o","p","r","s","t","u","v",
-          "w","x","y","z"]:
+for i in ["a", "b", "c", "d", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o",
+          "p", "r", "s", "t", "u", "v", "w",
+          "x", "y", "z"]:
     print("{}".format(i), end="")
