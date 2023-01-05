7c55a636f90ba7aeb7a24361a7391670e23e3ef6
Delali Funani <dfunani@live.co.za>
2023-01-05 05:17:20 -0800

    addded

---
+++ 0x01-python-if_else_loops_functions/8-uppercase.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+def islower(c):
+    return ord(c) > 96 and ord(c) < 123
+
+def uppercase(str):
+    for char in str:
+        temp = ""
+        if islower(char):
+            temp = chr(ord(char) - 32)
+        else:
+            temp = char
+        print(temp, end="")
+    print()
