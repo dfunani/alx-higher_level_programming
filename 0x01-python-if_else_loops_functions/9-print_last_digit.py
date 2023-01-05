4531641c2f40e502675e4a1c47ed5a86d4140d8d
Delali Funani <dfunani@live.co.za>
2023-01-05 05:33:12 -0800

    added codestyle - fixed to number - neg

--- 0x01-python-if_else_loops_functions/9-print_last_digit.py
+++ 0x01-python-if_else_loops_functions/9-print_last_digit.py
@@ -2,5 +2,9 @@
 def print_last_digit(number):
     if type(number) != int:
         return
-    print(number % 10, end="")
-    return number % 10
+    if number >= 0:
+        print(number % 10, end="")
+        return number % 10
+    else:
+        print(-number % 10, end="")
+        return -number % 10
