8dca7b88e420281d93c98873d0c2c5cae9699f4f
Delali Funani <dfunani@live.co.za>
2023-01-05 04:49:49 -0800

    addded

---
+++ 0x01-python-if_else_loops_functions/6-print_comb3.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+res = []
+for i in range(0, 10):
+    for j in range(0, 10):
+        if i != j and str(i) + str(j) not in res and str(j) + str(i) not in res:
+            res.append(str(i) + str(j))
+            if i == 8 and j == 9:
+                print(str(i) + str(j))
+            else:
+                print(str(i) + str(j) + ", ", end="")
