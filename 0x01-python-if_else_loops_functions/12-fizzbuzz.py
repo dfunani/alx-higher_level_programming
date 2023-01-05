dc51837e9c2c6ded9c25ff62fd6b30cfeaf46ec3
Delali Funani <dfunani@live.co.za>
2023-01-05 05:50:02 -0800

    added fizzbuzz - 100

--- 0x01-python-if_else_loops_functions/12-fizzbuzz.py
+++ 0x01-python-if_else_loops_functions/12-fizzbuzz.py
@@ -1,13 +1,11 @@
 #!/usr/bin/python3
 def fizzbuzz():
     for i in range(1, 101):
-        if i % 3 == 0 and i % 5 == 0 and i != 100:
+        if i % 3 == 0 and i % 5 == 0:
             print("FizzBuzz", end=' ')
-        elif i % 3 == 0 and i != 100:
+        elif i % 3 == 0:
             print("Fizz", end=' ')
-        elif i % 5 == 0 and i != 100:
+        elif i % 5 == 0:
             print("Buzz", end=' ')
-        elif i == 100:
-            print(i)
         else:
             print(i, end=' ')
