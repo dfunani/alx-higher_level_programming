8b8f1caa4236158fa62700c21173afca53eb6a35
Delali Funani <dfunani@live.co.za>
2023-01-05 05:36:02 -0800

    added codestyle - fixed to number - neg - str fix - raise excpt

--- 0x01-python-if_else_loops_functions/9-print_last_digit.py
+++ 0x01-python-if_else_loops_functions/9-print_last_digit.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 def print_last_digit(number):
     if type(number) != int:
-        return ""
+        raise Exception
     if number >= 0:
         print(number % 10, end="")
         return number % 10
