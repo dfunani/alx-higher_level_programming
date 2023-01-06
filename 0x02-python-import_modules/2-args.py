41d7be70d367b0c79dba14ff82d1a0c23f966987
Delali Funani <dfunani@live.co.za>
2023-01-06 12:02:34 -0800

    added add - add func - chmod - if - readme - sub - argvs

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -3,7 +3,9 @@ from sys import argv
 if __name__ == "__main__":
     if len(argv) <= 1:
         print("{} arguments.".format(len(argv) - 1))
-    else:
+    elif len(argv) > 2:
         print("{} arguments:".format(len(argv) - 1))
+    else:
+        print("{} argument:".format(len(argv) - 1))
     for i in range(1, len(argv)):
         print("{}: {}".format(i, argv[i]))
