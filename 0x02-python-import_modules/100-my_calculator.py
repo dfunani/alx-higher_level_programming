1da9e863be62eba896d995d1bab36665cac79230
Delali Funani <dfunani@live.co.za>
2023-01-06 23:32:24 -0800

    added magic form - fixed 100

---
+++ 0x02-python-import_modules/100-my_calculator.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+from calculator_1 import add, sub, mul, div
+from sys import exit, argv
+if __name__ == '__main__':
+    if len(argv) != 4:
+        exit("Usage: ./100-my_calculator.py <a> <operator> <b>")
+    a = int(argv[1])
+    b = int(argv[3])
+    if argv[2] == "+":
+        print("{} + {} = {}".format(a, b, add(a, b)))
+    elif argv[2] == "-":
+        print("{} - {} = {}".format(a, b, sub(a, b)))
+    elif argv[2] == "*":
+        print("{} * {} = {}".format(a, b, mul(a, b)))
+    elif argv[2] == "/":
+        print("{} / {} = {}".format(a, b, div(a, b)))
+    else:
+        exit("Unknown operator. Available operators: +, -, * and /")
