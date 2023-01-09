69a1b92c71b5a298e77f1c873810dc371b1d8dc3
Delali Funani <dfunani@live.co.za>
2023-01-08 23:38:51 -0800

    added

--- 0x03-python-data_structures/3-print_reversed_list_integer.py
+++ 0x03-python-data_structures/3-print_reversed_list_integer.py
@@ -1,4 +1,3 @@
 #!/usr/bin/python3
-def print_list_integer(my_list=[]):
-    if isinstance(my_list, list):
-        [print("{:d}".format(x)) for x in reversed(my_list)]
+def print_reversed_list_integer(my_list=[]):
+    [print("{:d}".format(x)) for x in reversed(my_list)]
