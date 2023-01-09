c513a8fc2bf80a665a66165fb0475ba568b19d81
Delali Funani <dfunani@live.co.za>
2023-01-09 00:55:40 -0800

    added list delete

---
+++ 0x03-python-data_structures/11-delete_at.py
@@ -0,0 +1,7 @@
+#!/usr/bin/python3
+def delete_at(my_list=[], idx=0):
+    if idx < 0 or idx >= len(my_list):
+        return my_list
+    res = []
+    [res.append(i) for i in range(len(my_list)) if i != idx]
+    return res
