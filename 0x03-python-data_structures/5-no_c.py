eaef1efd4ffabbe255dcbec99d14672f9f3f4363
Delali Funani <dfunani@live.co.za>
2023-01-08 23:55:15 -0800

    added isinstance - copy - list - func name - new str - fixed st

--- 0x03-python-data_structures/5-no_c.py
+++ 0x03-python-data_structures/5-no_c.py
@@ -1,5 +1,7 @@
 #!/usr/bin/python3
 def no_c(my_string):
     newStr = ""
-    [newStr += str for str in my_string if str.lower() != "c"]
+    for str in my_string:
+        if str.lower() != "c":
+            newStr += str
     return newStr
