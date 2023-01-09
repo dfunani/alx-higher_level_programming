6409075915aab31a739060cd62934cf0ff01ede3
Delali Funani <dfunani@live.co.za>
2023-01-09 00:18:30 -0800

    added print matrix git push

---
+++ 0x03-python-data_structures/6-print_matrix_integer.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+def print_matrix_integer(matrix=[[]]):
+    for i in matrix:
+        for index, j in enumerate(i):
+            print("{:d}".format(j), end="")
+            if index < len(i) - 1:
+                print(" ", end="")
+        print()
