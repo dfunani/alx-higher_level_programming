dd18bc20541d46490b5ce561f363c45dd5c8586c
Delali Funani <dfunani@live.co.za>
2023-01-12 05:28:54 -0800

    added coman nums - codestyle

--- 0x04-python-more_data_structures/12-roman_to_int.py
+++ 0x04-python-more_data_structures/12-roman_to_int.py
@@ -8,22 +8,22 @@ def roman_to_int(roman_string):
             "X": 10,
             "L": 50,
             "C": 100,
-            "D" :500,
+            "D": 500,
             "M": 1000,
         }
     r = 0
-    for i,c in enumerate(roman_string):
+    for i, c in enumerate(roman_string):
         if c not in conversion:
             return 0
         try:
-            if  c == 'I' and roman_string[i + 1] in ["V", "X"]:
+            if c == 'I' and roman_string[i + 1] in ["V", "X"]:
                 r -= conversion['I']
             elif c == 'X' and roman_string[i + 1] in ["L", "C"]:
                 r -= conversion['X']
             elif c == 'C' and roman_string[i + 1] in ["D", "M"]:
                 r -= conversion['C']
             else:
-                r  += conversion[c]
-        except:
                 r += conversion[c]
+        except Exception:
+            r += conversion[c]
     return r
