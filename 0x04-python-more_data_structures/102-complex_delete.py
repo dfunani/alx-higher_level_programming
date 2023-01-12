b45b6241ba9f242f2df4d59908269acc27636e25
Delali Funani <dfunani@live.co.za>
2023-01-12 04:49:45 -0800

    added return new dict

--- 0x04-python-more_data_structures/102-complex_delete.py
+++ 0x04-python-more_data_structures/102-complex_delete.py
@@ -1,4 +1,5 @@
 #!/usr/bin/python3
 def complex_delete(a_dictionary, value):
-    [a_dictionary.pop(key) for key in a_dictionary if key == value]
+    a_dict = a_dictionary.copy()
+    [a_dictionary.pop(key) for key in a_dict if a_dict[key] == value]
     return a_dictionary
