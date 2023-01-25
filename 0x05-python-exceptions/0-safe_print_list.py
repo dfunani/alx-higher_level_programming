5c989b1e65c40668c056bbad8f064e2ac95d03f5
Delali Funani <dfunani@live.co.za>
2023-01-25 05:47:35 -0800

    added print with sfety

---
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+
+def safe_print_list(my_list=[], x=0):
+    sum = 0
+    try:
+        for i, v in enumerate(my_list):
+            if i >= x:
+                break
+            print(my_list[i], end="")
+            sum += 1
+    except:
+        pass
+    print()
+    return sum
---
+++ 0x05-python-exceptions/README.md
@@ -0,0 +1,1 @@
+Project for the use of the try except or exception handling in general
