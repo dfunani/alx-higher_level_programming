1943e86236c85480b1d49443dafad14e3dfde7af
Delali Funani <dfunani@live.co.za>
2023-01-30 08:25:22 -0800

    added 1rec

--- 0x08-python-more_classes/1-rectangle.py
+++ 0x08-python-more_classes/1-rectangle.py
@@ -1,14 +1,16 @@
 #!/usr/bin/python3
-"""
-Rectangle class to serve as an abstraction of a Rectangle
+"""This module  has a class for rectangule instances.
+The module has the class Rectangle.
 """
 
 
 class Rectangle:
+    """class rectangle.
+    the class defines the height and width prived
+    instance attributes, getter and setter method for each.
     """
-    New rectangle can be created
-    """
-    def __init__(self, height=0, width=0):
+
+    def __init__(self, width=0, height=0):
         if (type(height) != int):
             raise TypeError("height must be an integer")
         elif (height < 0):
@@ -26,18 +28,18 @@ class Rectangle:
     def height(self):
         return self.__height
 
-    @property
-    def width(self):
-        return self.__width
-
     @height.setter
     def height(self, value):
         if (type(value) != int):
-            raise TypeError("width must be an integer")
+            raise TypeError("height must be an integer")
         elif (value < 0):
-            raise ValueError("width must be >= 0")
+            raise ValueError("height must be >= 0")
         else:
             self.__height = value
+
+    @property
+    def width(self):
+        return self.__width
 
     @width.setter
     def width(self, value):
