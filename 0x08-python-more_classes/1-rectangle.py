59f08d3b6c9be7058ba886113dc35bb528eb3cd4
Delali Funani <dfunani@live.co.za>
2023-01-30 08:18:14 -0800

    addexd rec class with get and set - type checks - type check on constructor

--- 0x08-python-more_classes/1-rectangle.py
+++ 0x08-python-more_classes/1-rectangle.py
@@ -9,8 +9,18 @@ class Rectangle:
     New rectangle can be created
     """
     def __init__(self, height=0, width=0):
-        self.__height = height
-        self.__width = width
+        if (type(height) != int):
+            raise TypeError("height must be an integer")
+        elif (height < 0):
+            raise ValueError("height must be >= 0")
+        else:
+            self.__height = height
+        if (type(width) != int):
+            raise TypeError("width must be an integer")
+        elif (width < 0):
+            raise ValueError("width must be >= 0")
+        else:
+            self.__width = width
 
     @property
     def height(self):
