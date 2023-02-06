c497e116cf58beff91b04770d1622b61ceec0656
Delali Funani <dfunani@live.co.za>
2023-02-06 05:37:09 -0800

    added checker

--- 0x0A-python-inheritance/2-is_same_class.py
+++ 0x0A-python-inheritance/2-is_same_class.py
@@ -3,6 +3,11 @@
 creates class
 """
 
+
 def is_same_class(obj, a_class):
     """checks class"""
+    if not obj:
+        return False
+    if not a_class:
+        return False
     return obj is a_class
