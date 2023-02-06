a5a3c4474bd036c1aa0fc2a6c8c5f21fc0e362b5
Delali Funani <dfunani@live.co.za>
2023-02-06 07:12:20 -0800

    added format

--- 0x0A-python-inheritance/11-square.py
+++ 0x0A-python-inheritance/11-square.py
@@ -8,5 +8,9 @@ Rectangle = __import__('9-rectangle').Rectangle
 class Square(Rectangle):
     """ Square class"""
     def __init__(self, size):
+        """ init a class"""
         self.integer_validator("size", size)
         super().__init__(size, size)
+
+    def __str__(self):
+        return f"[Square] {width}/{height}"
