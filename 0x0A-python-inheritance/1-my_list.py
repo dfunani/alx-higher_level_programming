7aaa191a8c1837edcfef12034d7ecabbfe2d1e7d
Delali Funani <dfunani@live.co.za>
2023-02-06 05:29:15 -0800

    added tests - doxstring

--- 0x0A-python-inheritance/1-my_list.py
+++ 0x0A-python-inheritance/1-my_list.py
@@ -1,10 +1,11 @@
 #!/usr/bin/python3
-"""
-1-my_list module
+"""1-my_list module
+creates class
 """
 
 
 class MyList(list):
-
+    """ class my_list"""
     def print_sorted(self):
+        """ method to print"""
         print(sorted(self))
