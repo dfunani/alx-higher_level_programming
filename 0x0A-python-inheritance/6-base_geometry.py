99f16619cb0c8f800e4f5afa756d43fa2c35414a
Delali Funani <dfunani@live.co.za>
2023-02-06 06:09:28 -0800

    added base class for geo - betty

---
+++ 0x0A-python-inheritance/6-base_geometry.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+"""5-base_geometry module
+create class
+"""
+
+
+class BaseGeometry:
+    """ Creates a base class"""
+
+    def area(self):
+        """ raises Exception"""
+        raise Exception("area() is not implemented")
---
+++ 0x0A-python-inheritance/6-base_geometry.py~
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+"""5-base_geometry module
+create class
+"""
+
+
+class BaseGeometry:
+    """ Creates a base class"""
+    pass
