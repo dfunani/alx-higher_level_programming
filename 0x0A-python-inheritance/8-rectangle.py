24e098b371cd307e44992a8a5e64a3d82fd79977
Delali Funani <dfunani@live.co.za>
2023-02-06 06:33:35 -0800

    added check

--- 0x0A-python-inheritance/8-rectangle.py
+++ 0x0A-python-inheritance/8-rectangle.py
@@ -24,6 +24,6 @@ class Rectangle(BaseGeometry):
     def __init__(self, width, height):
         """ Init vclass"""
         if self.integer_validator("width", width):
-            self.width = width
+            self.__width = width
         if self.integer_validator("height", height):
-            self.height = height
+            self.__height = height
