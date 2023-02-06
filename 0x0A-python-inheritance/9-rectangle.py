ceafdf661be3aac7724d5c62719b2fa3c0ffce0b
Delali Funani <dfunani@live.co.za>
2023-02-06 06:52:33 -0800

    added check- py - fixed

---
+++ 0x0A-python-inheritance/9-rectangle.py
@@ -0,0 +1,25 @@
+#!/usr/bin/python3
+"""5-base_geometry module
+create class
+"""
+BaseGeometry = __import__('7-base_geometry').BaseGeometry
+
+
+class Rectangle(BaseGeometry):
+    """ Creates a rect class"""
+    def __init__(self, width, height):
+        """ Init vclass"""
+        self.integer_validator("width", width)
+        self.integer_validator("height", height)
+        self.__height = height
+        self.__width = width
+
+    def area(self):
+        """ Extends area method"""
+        return self.__height * self.__width
+
+    def __str__():
+        return f"[Rectangle] {self__width}/{self.__height}"
+
+    def __repr__(self):
+        return (f"[Rectangle] {self.__width}/{self.__height}")
