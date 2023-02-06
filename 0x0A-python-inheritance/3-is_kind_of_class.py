ee88a242b6512f4ae50bc9dd32565cf39e95c921
Delali Funani <dfunani@live.co.za>
2023-02-06 05:53:58 -0800

    added type check - final - iskind

---
+++ 0x0A-python-inheritance/3-is_kind_of_class.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+"""3-is_kind_of_class module
+creates a check
+"""
+
+
+def is_kind_of_class(obj, a_class):
+    """
+    checks kind
+    """
+    if not obj or not a_class:
+        return False
+    return isinstance(obj, a_class)
