4d82bebf6526cea1cda3fb9e103514b1c6e279c1
Delali Funani <dfunani@live.co.za>
2023-02-06 07:08:30 -0800

    added h

--- 0x0A-python-inheritance/10-square.py
+++ 0x0A-python-inheritance/10-square.py
@@ -8,4 +8,5 @@ Rectangle = __import__('9-rectangle').Rectangle
 class Square(Rectangle):
     """ Square class"""
     def __init__(self, size):
+        self.integer_validator("size", size)
         super().__init__(size, size)
