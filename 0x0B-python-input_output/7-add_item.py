c25dd873aa6bb8b16c2502c8e9912578d729c755
Delali Funani <dfunani@live.co.za>
2023-02-07 07:29:41 -0800

    added list add file manager

--- 0x0B-python-input_output/7-add_item.py
+++ 0x0B-python-input_output/7-add_item.py
@@ -10,7 +10,7 @@ save_to_json_file = __import__('5-save_to_json_file').save_to_json_file
 filename = "add_item.json"
 try:
     my_list = load_from_json_file(filename)
-except:
+except Exception:
     my_list = []
 [my_list.append(i) for i in sys.argv[1:]]
 save_to_json_file(my_list, filename)
