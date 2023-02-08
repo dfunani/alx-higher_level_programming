634e2561d42cc308c214dfe90cf640e0ebc5f328
Delali Funani <dfunani@live.co.za>
2023-02-08 00:03:22 -0800

    added 9 to json - not private att git push

--- 0x0B-python-input_output/9-student.py
+++ 0x0B-python-input_output/9-student.py
@@ -7,9 +7,9 @@ create student class
 class Student:
     """class to student"""
     def __init__(self, first_name, last_name, age):
-        self.__first_name = first_name
-        self.__last_name = last_name
-        self.__age = age
+        self.first_name = first_name
+        self.last_name = last_name
+        self.age = age
 
     def to_json(self):
         """ to json"""
