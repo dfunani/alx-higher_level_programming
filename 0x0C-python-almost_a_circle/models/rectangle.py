6b556e0fd958856d9cb523badfe8143107196efa
root <dfunani>
2023-02-14 09:25:30 -0800

    added constructor check- pycode

--- 0x0C-python-almost_a_circle/models/rectangle.py
+++ 0x0C-python-almost_a_circle/models/rectangle.py
@@ -12,13 +12,13 @@ class Rectangle(Base):
     def __init__(self, width, height, x=0, y=0, id=None):
         """ constr class and inherit from base """
         super().__init__(id)
-        if Rectangle.check_type(width, "width") and Rectangle.check_val(width, "width"):
+        if Rectangle.c_type(width, "width") and Rectangle.c_val(width, "width"):
             self.__width = width
-        if Rectangle.check_type(height, "height") and Rectangle.check_val(height, "height"):
+        if Rectangle.c_type(height, "height") and Rectangle.c_val(height, "height"):
             self.__height = height
-        if Rectangle.check_type(x, "x") and Rectangle.check_val(x, "x"):
+        if Rectangle.c_type(x, "x") and Rectangle.c_val(x, "x"):
             self.__x = x
-        if Rectangle.check_type(y, "y") and Rectangle.check_val(y, "y"):
+        if Rectangle.c_type(y, "y") and Rectangle.c_val(y, "y"):
             self.__y = y
 
     @property
@@ -39,32 +39,32 @@ class Rectangle(Base):
 
     @width.setter
     def width(self, val):
-        if Rectangle.check_type(val, "width") and Rectangle.check_val(val, "width"):
+        if Rectangle.c_type(val, "width") and Rectangle.c_val(val, "width"):
             self.__width = val
 
     @height.setter
     def height(self, val):
-        if Rectangle.check_type(val, "height") and Rectangle.check_val(val, "height"):
+        if Rectangle.c_type(val, "height") and Rectangle.c_val(val, "height"):
             self.__height = val
 
     @x.setter
     def x(self, val):
-        if Rectangle.check_type(val, "x") and Rectangle.check_val(val, "x"):
+        if Rectangle.c_type(val, "x") and Rectangle.c_val(val, "x"):
             self.__x = val
 
     @y.setter
     def y(self, val):
-        if Rectangle.check_type(val, "y") and Rectangle.check_val(val, "y"):
+        if Rectangle.c_type(val, "y") and Rectangle.c_val(val, "y"):
             self.__y = val
 
     @staticmethod
-    def check_type(val, attr):
+    def c_type(val, attr):
         if type(val) is int:
             return True
         raise TypeError(f"{attr} must be an integer")
 
     @staticmethod
-    def check_val(val, attr):
+    def c_val(val, attr):
         if val <= 0 and (attr == "width" or attr == "height"):
             raise ValueError(f"{attr} must be > 0")
         if val < 0 and (attr == "x" or attr == "y"):
