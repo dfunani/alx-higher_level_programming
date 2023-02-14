ae6ec2f67d58e34dad693e054914c2845aa5e4c3
root <dfunani>
2023-02-14 07:05:08 -0800

    added Base class and constr

---
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -0,0 +1,20 @@
+"""base
+create a base class for inheritence by higher order classes
+"""
+
+
+class Base:
+    """
+    Base class: Parent Class
+    """
+    __nb_objects = 0
+    def __init__(self, id=None):
+        """
+        Constructor: creates the instance
+        """
+        if id:
+            self.id = id
+        else:
+            Base.__nb_objects += 1
+            self.id = Base.__nb_objects
+        
