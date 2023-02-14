91d026a7acf73eb2cf64ae3c378f92d7d85d2ecc
root <dfunani>
2023-02-14 11:42:39 -0800

    added square only - val

--- 0x0C-python-almost_a_circle/models/square.py
+++ 0x0C-python-almost_a_circle/models/square.py
@@ -11,10 +11,34 @@ class Square(Rectangle):
 
     def __init__(self, size, x=0, y=0, id=None):
         """ constr class and inherit from base """
+        if check_t(size, "size") and check_v(size, "size"):
+            self.__size = size
         super().__init__(size, size, x, y, id)
-        self.__size = size
 
     def __str__(self):
         """ str rep of the rect """
         a = f"{self.x}/{self.y}"
         return f"[Square] ({self.id}) {a} - {self.size}"
+
+    @property
+    def size(self):
+        return self.__size
+
+    @size.setter
+    def size(self, val):
+        if check_t(val, "size") and check_v(val, "size"):
+            self.__size = val
+
+    @staticmethod
+    def check_t(val, attr):
+        if type(val) is int:
+            return True
+        raise TypeError(f"{attr} must be an integer")
+
+    @staticmethod
+    def check_v(val, attr):
+        if val <= 0 and attr == "size":
+            raise ValueError(f"{attr} must be > 0")
+        if val < 0 and (attr == "x" or attr == "y"):
+            raise ValueError(f"{attr} must be >= 0")
+        return True
