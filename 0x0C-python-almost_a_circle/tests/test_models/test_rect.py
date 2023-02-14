2683d87aba3570c78e456b97bff04ac7d35e4cce
root <dfunani>
2023-02-14 08:25:58 -0800

    added rect class

---
+++ 0x0C-python-almost_a_circle/tests/test_models/test_rect.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+""" test_rect
+tests the rect class
+"""
+
+import unittest
+from models.rectangle import Rectangle
+
+
+class Test_Rectangle(unittest.TestCase):
+    """ class for testing the rect class """
+
+    pass
