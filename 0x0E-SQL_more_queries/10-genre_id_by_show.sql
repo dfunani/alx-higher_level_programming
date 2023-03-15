16be77891b45b2137fa9952d38f38f3ab7c4534e
Delali Funani <dfunani@live.co.za>
2023-03-15 11:11:33 -0700

    added script

---
+++ 0x0E-SQL_more_queries/10-genre_id_by_show.sql
@@ -0,0 +1,5 @@
+-- 0;10;1c
+SELECT tv_shows.title, tv_show_genres.genre_id
+FROM tv_shows INNER JOIN tv_show_genres
+ON tv_shows.id = tv_show_genres.genre_id
+ORDER BY genre_id, tv_show_genres.genre_id
