46c7c42652d91394ac5977271546275bbeb3e940
Delali Funani <dfunani@live.co.za>
2023-03-15 11:02:55 -0700

    ambiguity

--- 0x0E-SQL_more_queries/9-cities_by_state_join.sql
+++ 0x0E-SQL_more_queries/9-cities_by_state_join.sql
@@ -1,6 +1,6 @@
 -- join tables
 SELECT cities.id, cities.name, states.name
-  FROM hbtn_0d_usa.cities AS cities
-       INNER JOIN hbtn_0d_usa.states AS states
+  FROM cities AS cities
+       INNER JOIN states AS states
        ON cities.state_id = states.id
  ORDER BY cities.id;
