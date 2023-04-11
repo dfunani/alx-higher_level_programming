072a4a4a087be2fdc73c5b2c3a2a9826118c4056
root <dfunani>
2023-04-11 03:59:46 -0700

    added 7 multi

--- 0x12-javascript-warm_up/4-concat.js
+++ 0x12-javascript-warm_up/4-concat.js
@@ -1,3 +1,3 @@
 #!/usr/bin/node
 
-console.log(process.argv[2] + " is " + process.argv[2]);
+console.log(process.argv[2] + ' is ' + process.argv[3]);
---
+++ 0x12-javascript-warm_up/6-multi_languages_loop.js
@@ -0,0 +1,4 @@
+#!/usr/bin/node
+
+const arr = ['C is fun', 'Python is cool', 'JavaScript is amazing'];
+for (const i of arr) { console.log(i); }
