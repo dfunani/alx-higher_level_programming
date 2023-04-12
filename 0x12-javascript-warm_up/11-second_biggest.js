a50d6dc1f314e34b88ccf89127dab91a29702deb
root <dfunani>
2023-04-12 03:54:10 -0700

    added token

--- 0x12-javascript-warm_up/11-second_biggest.js
+++ 0x12-javascript-warm_up/11-second_biggest.js
@@ -3,7 +3,7 @@
 if (process.argv.length <= 3) {
   console.log(0);
 } else {
-  const res = process.argv.slice(3, process.argv.length);
+  const res = process.argv.slice(2, process.argv.length);
   res.sort((a, b) => parseInt(a) - parseInt(b));
   res.reverse();
   console.log(res[1]);
