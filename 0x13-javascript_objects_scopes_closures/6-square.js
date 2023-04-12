fc9fd01c0c5a82f567916ef7ee4b6df99b6c4fc6
root <dfunani>
2023-04-12 09:26:55 -0700

    added 6sqaure

---
+++ 0x13-javascript_objects_scopes_closures/6-square.js
@@ -0,0 +1,21 @@
+#!/usr/bin/node
+module.exports = class Square extends require('5-square.js') {
+    charPrint(c) {
+	if (!c)
+	{
+	    super.print()
+	}
+	else
+	{
+	    for (let i = 0; i < this.height; i++)
+	    {
+		let res = '';
+		for (let j = 0; j < this.width; j++)
+		{
+		    res += c;
+		}
+		console.log(res);
+	    }
+	}
+    }
+};
