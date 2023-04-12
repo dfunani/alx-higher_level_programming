35c3bfe6340631bc2d9c503ea0976a8dc54d1ac3
root <dfunani>
2023-04-12 04:00:43 -0700

    addd

--- 0x12-javascript-warm_up/101-call_me_moby.js
+++ 0x12-javascript-warm_up/101-call_me_moby.js
@@ -1,9 +1,7 @@
 #!/usr/bin/node
 
-exports.callMeMoby = function (number, theFunction)
-{
-    for (let i = 0; i < number; i++)
-    {
-	theFunction();
-    }
-}
+exports.callMeMoby = function (number, theFunction) {
+  for (let i = 0; i < number; i++) {
+    theFunction();
+  }
+};
