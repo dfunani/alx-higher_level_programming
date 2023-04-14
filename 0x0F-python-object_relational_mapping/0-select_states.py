d5e433fae947c5a73595fc4faad513c9e64b4e30
Delali Funani <dfunani@live.co.za>
2023-04-14 07:25:58 -0700

    added params - str

--- 0x0F-python-object_relational_mapping/0-select_states.py
+++ 0x0F-python-object_relational_mapping/0-select_states.py
@@ -14,6 +14,9 @@ if __name__ == "__main__":
 
     return: None
     """
+    import sys
+    import MySQLdb
+
     db = MySQLdb(host='localhost', port=3306, user=username, passwd=password,
                  db=name, charset=charset)
     cursor = db.cursor()
