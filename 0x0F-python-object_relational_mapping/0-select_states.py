35ad62e65510eb6bda6235177549e2a301001c7c
Delali Funani <dfunani@live.co.za>
2023-04-14 07:14:30 -0700

    added params

--- 0x0F-python-object_relational_mapping/0-select_states.py
+++ 0x0F-python-object_relational_mapping/0-select_states.py
@@ -31,5 +31,5 @@ def main(username, password, name):
 
 
 if __name__ == '__main__':
-    if len(sys.argv) == 3:
-        main()
+    if len(sys.argv) == 4:
+        main(sys.argv[1], sys.argv[2], sys.argv[3])
