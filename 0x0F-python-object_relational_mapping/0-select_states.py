9c231c2145bf75aaa57ffb7ea780e28d02140593
Delali Funani <dfunani@live.co.za>
2023-04-14 07:24:56 -0700

    added params - str

--- 0x0F-python-object_relational_mapping/0-select_states.py
+++ 0x0F-python-object_relational_mapping/0-select_states.py
@@ -2,12 +2,9 @@
 """*.py
 Application for running sql scripts either using
 ORM or DDL"""
-localhost = 'localhost'
-port = '3306'
-charset = 'utf8'
 
 
-def main(username, password, name):
+if __name__ == "__main__":
     """Project-Entry
     Description: runs a sql query
 
@@ -17,18 +14,10 @@ def main(username, password, name):
 
     return: None
     """
-    db = MySQLdb(host=localhost, port=port, user=username, passwd=password,
+    db = MySQLdb(host='localhost', port=3306, user=username, passwd=password,
                  db=name, charset=charset)
     cursor = db.cursor()
     cursor.execute(f"SELECT * FROM states ORDER BY states.id ASC;")
     result = cursor.fetchall()
     for i, elem in enumerate(result, 1):
         print(elem)
-    return
-
-
-if __name__ == '__main__':
-    import sys
-    import MySQLdb
-
-    main(sys.argv[1], sys.argv[2], sys.argv[3])
