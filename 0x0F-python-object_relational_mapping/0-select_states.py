822d7b5609a41c165f6591d6048174ae9f88c00f
Delali Funani <dfunani@live.co.za>
2023-04-14 07:28:35 -0700

    added params - str

--- 0x0F-python-object_relational_mapping/0-select_states.py
+++ 0x0F-python-object_relational_mapping/0-select_states.py
@@ -1,26 +1,16 @@
 #!/usr/bin/python3
-"""0-select_states.py
-Application for running sql scripts either using
-ORM or DDL"""
+"""lists all states from the database hbtn_0e_0_usa"""
 
+if __name__ == '__main__':
 
-if __name__ == "__main__":
-    """Project-Entry
-    Description: runs a sql query
-
-    username: __str__
-    password: __str__
-    name: __str__
-
-    return: None
-    """
-    import sys
     import MySQLdb
+    import sys
 
-    db = MySQLdb(host='localhost', port=3306, user=username, passwd=password,
-                 db=name, charset=charset)
-    cursor = db.cursor()
-    cursor.execute(f"SELECT * FROM states ORDER BY states.id ASC;")
-    result = cursor.fetchall()
-    for i, elem in enumerate(result, 1):
-        print(elem)
+    db = MySQLdb.connect(host='localhost', port=3306,
+                         user=sys.argv[1], passwd=sys.argv[2], db=sys.argv[3])
+
+    cur = db.cursor()
+    cur.execute("SELECT * FROM states ORDER BY states.id ASC;")
+    rows = cur.fetchall()
+    for row in rows:
+        print(row)
