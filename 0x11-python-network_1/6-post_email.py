e8534823e6d23477863aa880b7ff2c90b8705cee
Delali Funani <dfunani@live.co.za>
2023-05-02 12:51:54 -0700

    added post majol

--- 0x11-python-network_1/6-post_email.py
+++ 0x11-python-network_1/6-post_email.py
@@ -1,5 +1,8 @@
 #!/usr/bin/python3
 """ Module covers a significant component of web servers and clients """
+import sys
+import requests
 
 if __name__ == "__main__":
-    return
+    response = requests.post(sys.argv[1], data={"email": sys.argv[2]})
+    print(response.text)
