787dfda90d26518d9b135a97e12f1e960c965276
Delali Funani <dfunani@live.co.za>
2023-05-02 13:08:15 -0700

    added status code

--- 0x11-python-network_1/8-json_api.py
+++ 0x11-python-network_1/8-json_api.py
@@ -1,5 +1,13 @@
 #!/usr/bin/python3
 """ Module covers a significant component of web servers and clients """
+import sys
+import requests
 
 if __name__ == "__main__":
-    return
+    res = requests.post(sys.argv[1], data={"q": sys.argv[2]})
+    if not res.json():
+        print("No result")
+    elif 'id' not in res.json() or 'name' not in res.json():
+        print("Not a valid JSON")
+    else:
+        print(f"[{res.json().get('id')}] {res.json().get('name')}")
