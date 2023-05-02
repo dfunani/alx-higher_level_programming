64be754a3b96fd5a0c9327b404055f82b8743427
Delali Funani <dfunani@live.co.za>
2023-05-02 12:58:28 -0700

    added status code

--- 0x11-python-network_1/7-error_code.py
+++ 0x11-python-network_1/7-error_code.py
@@ -1,5 +1,11 @@
 #!/usr/bin/python3
 """ Module covers a significant component of web servers and clients """
+import sys
+import requests
 
 if __name__ == "__main__":
-    return
+    response = requests.get(sys.argv[1])
+    if response.status_code >= 400:
+        print(f"Error code: {response.status_code}")
+    else:
+        print(response.text)
