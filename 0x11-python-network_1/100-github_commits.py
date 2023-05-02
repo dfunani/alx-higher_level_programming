ad432b4768c468c9708fedaf3ac679c00453a883
Delali Funani <dfunani@live.co.za>
2023-05-02 13:25:10 -0700

    added

--- 0x11-python-network_1/100-github_commits.py
+++ 0x11-python-network_1/100-github_commits.py
@@ -1,5 +1,10 @@
 #!/usr/bin/python3
 """ Module covers a significant component of web servers and clients """
+import sys
+from sys import argv as a
 
 if __name__ == "__main__":
-    return
+    api = f"https://api.github.com/repos/{a[2]}/{a[1]}/commits?per_page=10"
+    commits = requests.get(api).json()
+    for commit in commits:
+        print(f"{commit.get('sha')}: {commit.get('commit').get('author').get('name')}")
