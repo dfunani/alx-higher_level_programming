35ca866a8ea05dcae17c46d9dd9f121149c6dcd3
Delali Funani <dfunani@live.co.za>
2023-05-02 11:49:41 -0700

    added checks

--- 0x11-python-network_1/2-post_email.py
+++ 0x11-python-network_1/2-post_email.py
@@ -1,5 +1,8 @@
 #!/usr/bin/python3
 """ Module covers a significant component of web servers and clients """
+from urllib import request, parse
+import sys
 
 if __name__ == "__main__":
-    return
+    with request.urlopen(sys.argv[1], data=parse.urlencode({"email": sys.argv[2]}).encode('utf-8')) as response:
+        print(response.read().decode('utf-8'))
