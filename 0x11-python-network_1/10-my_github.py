3705a7a5ec3d2dc58037037b54fee31b3ab4df8a
Delali Funani <dfunani@live.co.za>
2023-05-02 13:16:43 -0700

    added status code

--- 0x11-python-network_1/10-my_github.py
+++ 0x11-python-network_1/10-my_github.py
@@ -1,5 +1,9 @@
 #!/usr/bin/python3
 """ Module covers a significant component of web servers and clients """
+import sys
+import requests
 
 if __name__ == "__main__":
-    return
+    response = requests.get("https://api.github.com/user",
+                            auth=(sys.argv[1], sys.argv[2]))
+    print(response.json().get('id'))
