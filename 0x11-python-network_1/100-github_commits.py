7627f61192a56e1c30bfb216fb41b300789209c3
Delali Funani <dfunani@live.co.za>
2023-05-02 13:26:18 -0700

    added

--- 0x11-python-network_1/100-github_commits.py
+++ 0x11-python-network_1/100-github_commits.py
@@ -6,5 +6,5 @@ from sys import argv as a
 if __name__ == "__main__":
     api = f"https://api.github.com/repos/{a[2]}/{a[1]}/commits?per_page=10"
     commits = requests.get(api).json()
-    for commit in commits:
-        print(f"{commit.get('sha')}: {commit.get('commit').get('author').get('name')}")
+    for c in commits:
+        print(f"{c.get('sha')}: {c.get('commit').get('author').get('name')}")
