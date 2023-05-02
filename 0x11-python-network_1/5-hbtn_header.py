c7e9ba10264ed0d2b21066986ae5e961b4fbebd7
Delali Funani <dfunani@live.co.za>
2023-05-02 12:41:51 -0700

    added headers .get

--- 0x11-python-network_1/5-hbtn_header.py
+++ 0x11-python-network_1/5-hbtn_header.py
@@ -1,5 +1,7 @@
 #!/usr/bin/python3
 """ Module covers a significant component of web servers and clients """
+import requests, sys
 
 if __name__ == "__main__":
-    return
+    response = requests.get(sys.argv[1]).headers
+    print(response.get('X-Request-Id'))
