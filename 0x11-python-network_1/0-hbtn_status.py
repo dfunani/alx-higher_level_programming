f0f0ad6ba5581c0af4d89953a614e6c61fba4a8d
Delali Funani <dfunani@live.co.za>
2023-05-02 11:38:02 -0700

    addded id traclker - chmod

--- 0x11-python-network_1/0-hbtn_status.py
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -5,7 +5,7 @@ from urllib import request
 if __name__ == "__main__":
     with request.urlopen("https://alx-intranet.hbtn.io/status") as response:
         body = response.read()
-        print(f"""Body response:
-    - type: type({body})
-    - content: {body}
-    - utf8 content: {body.decode('utf-8')}""")
+        print(f"Body response:")
+        print(f"\t- type: type({body}")
+        print(f"\t- content: {body}")
+        print(f"\t- utf8 content: {body.decode('utf-8')}")
