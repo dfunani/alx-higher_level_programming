a75726c3efeb18c25c0c2d59e3a4667fdccb062a
Delali Funani <dfunani@live.co.za>
2023-05-02 12:35:34 -0700

    added req lib

--- 0x11-python-network_1/4-hbtn_status.py
+++ 0x11-python-network_1/4-hbtn_status.py
@@ -1,9 +1,9 @@
 #!/usr/bin/python3
-""" Module covers a significant component of web servers and clients """
+"""Module covers a significant component of web servers and clients """
 import requests
 
 if __name__ == "__main__":
     response = requests.get("https://alx-intranet.hbtn.io/status").text
     print(f"""Body response:
-    - type: {type(response)}
-    - content: {response}""")
+\t- type: {type(response)}
+\t- content: {response}""")
