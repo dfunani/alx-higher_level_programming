0c3944de559d447e73d4c7d739f39165f2fae018
Delali Funani <dfunani@live.co.za>
2023-05-02 12:23:52 -0700

    added

--- 0x11-python-network_1/3-error_code.py
+++ 0x11-python-network_1/3-error_code.py
@@ -1,5 +1,11 @@
 #!/usr/bin/python3
 """ Module covers a significant component of web servers and clients """
+from urllib import request, error
+import sys
 
 if __name__ == "__main__":
-    return
+    try:
+        with request.urlopen(sys.argv[1]) as response:
+            print(response.read().decode('utf-8'))
+    except error.HTTPError as e:
+        print(f"Error code: {e.code}")
